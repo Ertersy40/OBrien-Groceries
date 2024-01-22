96eb42382548be5840b866127492b88bdf41cd3d
Ertersy40 <wonk4040@gmail.com>
2024-01-22 19:34:13 +1100

    Fixed parents names

--- Transactions.js
+++ Transactions.js
@@ -6,7 +6,7 @@ const names = {
     'Mr William Charles O': 'Will',
     'Ms Emily Maude O': "Em",
     'Mr Bryce Kotas': 'Bryce',
-    'Lisal and Chuck O': 'Mum & Dad',
+    "C D O'Brien & L J DO": 'Mum & Dad',
 }
 
 function fetchTransactions() {
